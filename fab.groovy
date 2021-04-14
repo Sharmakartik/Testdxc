7500648d5e8049110bb5ba2a81cc3fb531ae78b6
Sharmakartik <Kartik.gs@outlook.com>
2021-04-14 17:44:29 +0530

    fixed

--- fab.groovy
+++ fab.groovy
@@ -1,24 +1,36 @@
 
-def fabresult(number){
+// fabresult, will print first n number's
+//@params n : Number of element in fab. series
+
+def fabresult(n){ 
+ // initialize
  def a = 0
- def b = 0 
- der result = '0,1,'
+ def b = 1
+ def result = '0,1,'
  
-  if(number == 0){
+  if(n == 0){
+    result = ''
+  }else if(n == 1){
     result = '0'
-  }elseif(number == 0){
+  }else if(n == 2){
     result = '0,1'
   }else{
     def c = 0
-    for(i=0 ; i<number ; i++){
-      c = a +b
+    for(i=0 ; i< n-2 ; i++){
+      c = a + b
       a = b
       b = c
-      if(i == n-1){
-        
+      if(i == n-3){
+       result += +c
+      }else{
+        result+= c+','
       }
     }
   }
-  
-
+  print(result)
 }
+
+// calling the function fabresult
+   // @params number : Number of element in fab. series
+
+fabresult(8)
