3298b4164381923ffcd9b180ee1c774bc02fccb2
Sharmakartik <Kartik.gs@outlook.com>
2021-04-14 17:25:03 +0530

    initial changes

---
+++ fab.groovy
@@ -0,0 +1,24 @@
+
+def fabresult(number){
+ def a = 0
+ def b = 0 
+ der result = '0,1,'
+ 
+  if(number == 0){
+    result = '0'
+  }elseif(number == 0){
+    result = '0,1'
+  }else{
+    def c = 0
+    for(i=0 ; i<number ; i++){
+      c = a +b
+      a = b
+      b = c
+      if(i == n-1){
+        
+      }
+    }
+  }
+  
+
+}
