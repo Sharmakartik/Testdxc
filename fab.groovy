9da7c19cca926d7e1b07bf044cd987de3485047f
Sharmakartik <Kartik.gs@outlook.com>
2021-04-15 00:27:31 +0530

    added assert statements for testing the fabresult() api

--- fab.groovy
+++ fab.groovy
@@ -2,8 +2,7 @@
 // fabresult, will print first n number's
 //@params n : Number of element in fab. series
 
-def fabresult(n){ 
- // initialize
+def fabresult(n){  // initialize
  def a = 0
  def b = 1
  def result = '0,1,'
@@ -26,11 +25,19 @@ def fabresult(n){
         result+= c+','
       }
     }
-  }
-  print(result)
+   }
+  return result
 }
 
-// calling the function fabresult
-   // @params number : Number of element in fab. series
+// Can't add the test cases, but check's the functionaltiy of above api, using assertion
+def fabresult_2 = fabresult(2)
+def fabresult_8 = fabresult(8) // calling the function with 8 as an argument in fabresult() api to get first 8 element in fab. series
+def fabresult_9 = fabresult(9)
+def fabresult_11 = fabresult(11)
+def fabresult_12 = fabresult(12)
 
-fabresult(8)
+assert fabresult_2 == '0,1' : "not equal, failed when displaying first 2 elements"
+assert fabresult_8 == '0,1,1,2,3,5,8,13' : "not equal, failed while displaying first 8 elements"
+assert fabresult_9 == '0,1,1,2,3,5,8,13,21' : "not equal, failed while displaying first 9 elements"
+assert fabresult_11 == '0,1,1,2,3,5,8,13,21,34,55' : "not equal, failed while displaying first 11 elements"
+assert fabresult_12 == '0,1,1,2,3,5,8,13,21,34,55,89' : "not equal, failed while displaying first 12 elements"
