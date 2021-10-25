43f053c680d6277085be1f4a041418438a618190
Sharmakartik <Kartik.gs@outlook.com>
2021-10-25 13:11:14 +0530

    terraformec2.tf

---
+++ terraformec2.tf
@@ -0,0 +1,148 @@
+provider "aws" {
+	region = "us-east-1"
+}
+
+resource "aws_key_pair" "terraform-demo" {
+  key_name   = "mykey"
+  public_key = "${file("mykey.pub")}"
+}
+
+resource "aws_instance" "my-instance" {
+	ami = "ami-04169656fea786776"
+	instance_type = "t2.nano"
+	key_name = "${aws_key_pair.terraform-demo.key_name}"
+	user_data = file("install.sh")
+	# the VPC subnet
+    subnet_id = aws_subnet.main-public-1.id
+
+    # the security group
+    vpc_security_group_ids = [aws_security_group.allow-ssh.id, aws_security_group.allow-https.id, aws_security_group.allow-http.id]
+
+	tags = {
+		Name = "Terraform"	
+		Batch = "5AM"
+	}
+}
+
+output "ip" {
+  value = aws_instance.my-instance.public_ip
+}
+
+# Internet VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  enable_classiclink   = "false"
+  tags = {
+    Name = "main"
+  }
+}
+
+# Subnets
+resource "aws_subnet" "main-public-1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "main-public-1"
+  }
+}
+
+# Internet GW
+resource "aws_internet_gateway" "main-gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+# route tables
+resource "aws_route_table" "main-public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main-gw.id
+  }
+
+  tags = {
+    Name = "main-public-1"
+  }
+}
+
+# route associations public
+resource "aws_route_table_association" "main-public-1-a" {
+  subnet_id      = aws_subnet.main-public-1.id
+  route_table_id = aws_route_table.main-public.id
+}
+
+resource "aws_security_group" "allow-ssh" {
+  vpc_id      = aws_vpc.main.id
+  name        = "allow-ssh"
+  description = "security group that allows ssh and all egress traffic"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow-ssh"
+  }
+}
+
+resource "aws_security_group" "allow-https" {
+  vpc_id      = aws_vpc.main.id
+  name        = "allow-https"
+  description = "security group that allows ssh and all egress traffic"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow-https"
+  }
+}
+
+resource "aws_security_group" "allow-http" {
+  vpc_id      = aws_vpc.main.id
+  name        = "allow-http"
+  description = "security group that allows ssh and all egress traffic"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow-http"
+  }
+}
+
